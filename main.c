96b492d446718f05f06650954ec5f794f6c9e53c
Daniel Ferreira de Melo <daniel.ferreira7d@gmail.com>
2026-09-08 15:08:17 -0300

    Implementação menu interativo e percistencia de arquivos

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ void ler_dados(Produto *p)
 
     printf("Digite o nome do produto: ");
     scanf("%s", p->nome);
-    
+
     printf("Qual o preco de custo do produto %s: ", p->nome);
     scanf("%f", &(p->valores.custo));
 
@@ -53,7 +53,8 @@ void imprimir_dados(Produto *p)
     }
 }
 
-void produto_mais_lucrativo(Produto p[], int quantidade_produtos) {
+void produto_mais_lucrativo(Produto p[], int quantidade_produtos)
+{
     int indice_mais_lucrativo = 0;
 
     for (int i = 1; i < quantidade_produtos; i++)
@@ -66,7 +67,8 @@ void produto_mais_lucrativo(Produto p[], int quantidade_produtos) {
     printf("O produto mais lucrativo e o %s com uma margem de %.0f%%\n", p[indice_mais_lucrativo].nome, p[indice_mais_lucrativo].valores.margem);
 }
 
-void filtrar_por_preco(Produto p[], int quantidade_produtos, float preco_limite) {
+void filtrar_por_preco(Produto p[], int quantidade_produtos, float preco_limite)
+{
     for (int i = 0; i < quantidade_produtos; i++)
     {
         if (p[i].valores.venda <= preco_limite)
@@ -76,7 +78,8 @@ void filtrar_por_preco(Produto p[], int quantidade_produtos, float preco_limite)
     }
 }
 
-void ordenar_por_preco(Produto p[], int quantidade_produtos) {
+void ordenar_por_preco(Produto p[], int quantidade_produtos)
+{
     Produto temp;
     int ok = 0;
 
@@ -95,49 +98,154 @@ void ordenar_por_preco(Produto p[], int quantidade_produtos) {
             }
         }
     }
-    
+
     printf("\n------Produtos ordenados por preco de venda------\n");
 
     for (int i = 0; i < quantidade_produtos; i++)
     {
         printf("-> [%d] %s - RS %.2f\n", p[i].codigo, p[i].nome, p[i].valores.venda);
     }
-    
+}
+
+void salvar_produtos(Produto p[], int quantidade_produtos)
+{
+    FILE *arquivo = fopen("produtos.txt", "w");
+
+    if (arquivo == NULL)
+    {
+        printf("Erro ao abrir o arquivo\n");
+        return;
+    }
+
+    for (int i = 0; i < quantidade_produtos; i++)
+    {
+        fprintf(arquivo, "%d %s %f %f\n", p[i].codigo, p[i].nome, p[i].valores.custo, p[i].valores.venda);
+    }
+
+    fclose(arquivo);
+    printf("Produtos salvos com sucesso\n");
+}
+
+int ler_produtos(Produto p[])
+{
+    FILE *arquivo = fopen("produtos.txt", "r");
+
+    if (arquivo == NULL)
+    {
+        return 0;
+    }
+
+    int i = 0;
+
+    while (fscanf(arquivo, "%d %s %f %f", &p[i].codigo, p[i].nome, &p[i].valores.custo, &p[i].valores.venda) == 4 && i < MAX_PRODUTOS)
+    {
+        p[i].valores.margem = ((p[i].valores.venda - p[i].valores.custo) / p[i].valores.venda) * 100;
+        i++;
+    }
+
+    fclose(arquivo);
+    return i;
 }
 
 int main()
 {
-    int quantidade_produtos;
+    int quantidade_produtos = 0;
     float preco_limite;
-
-    printf("Quantos produtos tem cadastrado dentro do seu sistema?\n");
-    scanf("%d", &quantidade_produtos);
+    int opcao;
 
     Produto p[MAX_PRODUTOS];
 
-    for (int i = 0; i < quantidade_produtos; i++)
+    quantidade_produtos = ler_produtos(p);
+
+    if (quantidade_produtos > 0)
     {
-        printf("\n ----------Produto %d ----------\n", i + 1);
-        p[i].valores.margem = -1.0;
-        ler_dados(&p[i]);
+        printf("Produtos carregados com sucesso! Total: %d\n", quantidade_produtos);
     }
 
-    for (int i = 0; i < quantidade_produtos; i++)
+    do
     {
-        calcular_lucro(&p[i]);
-    }
-    
-    for (int i = 0; i < quantidade_produtos; i++)
-    {
-        imprimir_dados(&p[i]);
-    }
+        printf("---------------------System products---------------------\n\n");
 
-    produto_mais_lucrativo(p, quantidade_produtos);
+        printf("Escolha uma das opcoes abaixo:\n\n");
+        printf("Opcao 1: Mostrar produtos cadastrados\n");
+        printf("Opcao 2: Cadastrar um novo produto\n");
+        printf("Opcao 3: Ver produto mais lucrativo\n");
+        printf("Opcao 4: Filtrar por preco\n");
+        printf("Opcao 5: Ordenar por preco\n");
+        printf("Opcao 6: Salvar\n");
+        printf("Caso queira sair tecle 0\n");
 
-    printf("Digite um limite de preco, para filtrarmos quais produtos passam pelo filtro: ");
-    scanf("%f", &preco_limite);
+        scanf("%d", &opcao);
 
-    filtrar_por_preco(p, quantidade_produtos, preco_limite);
-    ordenar_por_preco(p, quantidade_produtos);
+        switch (opcao)
+        {
+        case 1:
+            if (quantidade_produtos == 0)
+            {
+                printf("Nenhum produto cadastrado\n");
+            }
+            else
+            {
+                for (int i = 0; i < quantidade_produtos; i++)
+                {
+                    imprimir_dados(&p[i]);
+                }
+            }
+            break;
+        case 2:
+            if (quantidade_produtos < MAX_PRODUTOS)
+            {
+                printf("\n ---------- Novo Produto ----------\n");
+                ler_dados(&p[quantidade_produtos]);
+                calcular_lucro(&p[quantidade_produtos]);
+                quantidade_produtos++;
+                printf("Produto cadastrado com sucesso!\n");
+            }
+            else
+            {
+                printf("Limite de produtos atingido!\n");
+            }
+            break;
+        case 3:
+            if (quantidade_produtos > 0)
+                produto_mais_lucrativo(p, quantidade_produtos);
+            else
+                printf("Nenhum produto cadastrado.\n");
+            break;
+
+        case 4:
+            if (quantidade_produtos > 0)
+            {
+                printf("Digite um limite de preco: ");
+                scanf("%f", &preco_limite);
+                filtrar_por_preco(p, quantidade_produtos, preco_limite);
+            }
+            else
+            {
+                printf("Nenhum produto cadastrado.\n");
+            }
+            break;
+
+        case 5:
+            if (quantidade_produtos > 0)
+                ordenar_por_preco(p, quantidade_produtos);
+            else
+                printf("Nenhum produto cadastrado.\n");
+            break;
+
+        case 6:
+            salvar_produtos(p, quantidade_produtos);
+            break;
+
+        case 0:
+            salvar_produtos(p, quantidade_produtos);
+            break;
+        default:
+            printf("Opcao invalida, tente novamente!\n");
+            printf("Saindo do sistema...\n");
+        }
+
+    } while (opcao != 0);
+
     return 0;
 }
